5e0a65b22c5af52d63e335faae05d08e0e41944d
Alexander Olsson <alexander.olsson8@learnet.se>
2018-12-12 10:40:37 +0100

    Initial commit

---
+++ src/integral.java
@@ -0,0 +1,11 @@
+// 1.  Skriv in funktionen
+// 2.  Skriv in högsta värde
+// 3.  Skriv in lägsta värde
+// 4.  Beräkna integralen. Detta görs via en loop som börjar vid det minsta värdet och avslutas vid det största.
+
+
+public class integral {
+    public static void main(String[] args) {
+
+    }
+}
